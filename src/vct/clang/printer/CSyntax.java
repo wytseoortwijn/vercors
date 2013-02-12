196adbd835f413365713310bb9a99df615d17c3e
Stefan Blom <S.C.C.Blom@utwente.nl>
2013-02-12 15:04:00 +0100

    refactor clang parser as a plugin

---
+++ src/vct/clang/printer/CSyntax.java
@@ -0,0 +1,71 @@
+package vct.clang.printer;
+
+import vct.util.Syntax;
+import static vct.col.ast.StandardOperator.*;
+import static vct.col.ast.PrimitiveType.Sort.*;
+
+public class CSyntax {
+  private static Syntax syntax;
+  
+  public static Syntax get(){
+    if(syntax==null){
+      syntax=new Syntax();
+
+      syntax.addLeftFix(Mult,"*",130);
+      syntax.addLeftFix(Div,"/",130);
+      syntax.addLeftFix(Mod,"%",130);
+      
+      syntax.addLeftFix(Plus,"+",120);
+      syntax.addLeftFix(Minus,"-",120);
+      
+      syntax.addInfix(LT,"<",100);
+      syntax.addInfix(LTE,"<=",100);
+      syntax.addInfix(GT,">",100);
+      syntax.addInfix(GTE,">=",100);
+      
+      syntax.addRightFix(Assign,"=",30);
+    }
+    return syntax;
+  } 
+
+}
+
+/*
+
+17  ::  Scope resolution  Left-to-right
+16  ++   --   Suffix/postfix increment and decrement
+    ()  Function call
+    []  Array subscripting
+    .   Element selection by reference
+    −>  Element selection through pointer
+15  ++   --   Prefix increment and decrement  Right-to-left
+    +   −   Unary plus and minus
+    !   ~   Logical NOT and bitwise NOT
+    (type)  Type cast
+    *   Indirection (dereference)
+    &   Address-of
+    sizeof  Size-of
+    new, new[]  Dynamic memory allocation
+    delete, delete[]  Dynamic memory deallocation
+14  .*   ->*  Pointer to member   Left-to-right
+13   *   /   %   Multiplication, division, and remainder
+12   +   −   Addition and subtraction
+11   <<   >>   Bitwise left shift and right shift
+10   <   <=  For relational operators < and ≤ respectively
+    >   >=  For relational operators > and ≥ respectively
+ 9  ==   !=   For relational = and ≠ respectively
+ 8  &   Bitwise AND
+ 7  ^   Bitwise XOR (exclusive or)
+ 6  |   Bitwise OR (inclusive or)
+ 5  &&  Logical AND
+ 4  ||  Logical OR
+ 3  ?:  Ternary conditional   Right-to-left
+    =   Direct assignment (provided by default for C++ classes)
+    +=   −=   Assignment by sum and difference
+   *=   /=   %=  Assignment by product, quotient, and remainder
+    <<=   >>=   Assignment by bitwise left shift and right shift
+    &=   ^=   |=  Assignment by bitwise AND, XOR, and OR
+ 2 throw   Throw operator (for exceptions)
+ 1 ,   Comma   Left-to-right 
+
+*/
--- src/vct/util/Syntax.java
+++ src/vct/util/Syntax.java
@@ -3,6 +3,7 @@ package vct.util;
 
 import vct.col.ast.PrimitiveType.Sort;
 import vct.col.ast.StandardOperator;
+import vct.util.Syntax.Associativity;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -20,10 +21,22 @@ public class Syntax {
 
   public static enum Associativity{Left,Right,None};
 
-  private Map<StandardOperator,Integer> precedence_map = new EnumMap(StandardOperator.class);
-  private Map<StandardOperator,String[]> syntax_map = new EnumMap(StandardOperator.class);
-  private Map<StandardOperator,Associativity> associativity_map = new EnumMap(StandardOperator.class);
-  private Map<Sort,String> type_map = new EnumMap(Sort.class);
+  private Map<StandardOperator,Integer> precedence_map = new EnumMap<StandardOperator, Integer>(StandardOperator.class);
+  private Map<StandardOperator,String[]> syntax_map = new EnumMap<StandardOperator, String[]>(StandardOperator.class);
+  private Map<StandardOperator,Associativity> associativity_map = new EnumMap<StandardOperator, Associativity>(StandardOperator.class);
+  private Map<Sort,String> type_map = new EnumMap<Sort, String>(Sort.class);
+  
+  private Map<String,StandardOperator> operator_map = new HashMap<String, StandardOperator>();
+  
+  /**
+   * Convert a string to an operator.
+   * @param op String representation of an operator. 
+   * @return The standard operator the string represents, if it has been defined,
+   *         and null otherwise.
+   */
+  public StandardOperator parseOperator(String op){
+    return operator_map.get(op);
+  }
   
   /** Is the standard function op an operator in this language? */
   public boolean isOperator(StandardOperator op){
@@ -51,6 +64,7 @@ public class Syntax {
     String full_syntax[]={"",syntax,""};
     precedence_map.put(op,precedence);
     syntax_map.put(op,full_syntax);
+    operator_map.put(syntax,op);
     associativity_map.put(op,Associativity.Left);
   }
   /** Add a non-associative binary infix operator to the language. */
@@ -58,6 +72,7 @@ public class Syntax {
     String full_syntax[]={"",syntax,""};
     precedence_map.put(op,precedence);
     syntax_map.put(op,full_syntax);
+    operator_map.put(syntax,op);
     associativity_map.put(op,Associativity.None);
   }
   /** Add a prefix operator to the language. */
@@ -65,6 +80,7 @@ public class Syntax {
     String full_syntax[]={syntax,""};
     precedence_map.put(op,precedence);
     syntax_map.put(op,full_syntax);
+    operator_map.put(syntax,op);
     associativity_map.put(op,Associativity.None);
   }
   /** Add a postfix operator to the language. */
@@ -72,6 +88,7 @@ public class Syntax {
     String full_syntax[]={"",syntax};
     precedence_map.put(op,precedence);
     syntax_map.put(op,full_syntax);
+    operator_map.put(syntax,op);
     associativity_map.put(op,Associativity.None);
   }
 
@@ -80,6 +97,7 @@ public class Syntax {
     String full_syntax[]={"",syntax,""};
     precedence_map.put(op,precedence);
     syntax_map.put(op,full_syntax);
+    operator_map.put(syntax,op);
     associativity_map.put(op,Associativity.Right);
   }
   
