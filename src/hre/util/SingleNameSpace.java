3a689fca7191e63611b8a807d200011eba76be5d
Stefan Blom <S.C.C.Blom@utwente.nl>
2013-05-07 18:18:39 +0200

    initial work on generics

--- src/hre/util/SingleNameSpace.java
+++ src/hre/util/SingleNameSpace.java
@@ -1,7 +1,9 @@
 package hre.util;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
 
 /**
@@ -12,7 +14,7 @@ import java.util.Stack;
  * @param <K>
  * @param <D>
  */
-public class SingleNameSpace<K,D> {
+public class SingleNameSpace<K,D> implements Map<K,D> {
 
   private Stack<Map<K,D>> stack=new Stack<Map<K, D>>();
   
@@ -31,5 +33,54 @@ public class SingleNameSpace<K,D> {
   }
   public void leave(){
     map=stack.pop();
+  }
+  
+  @Override
+  public int size() {
+    return map.size();
+  }
+  @Override
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
+  @Override
+  public boolean containsKey(Object key) {
+     return map.containsKey(key);
+  }
+  @Override
+  public boolean containsValue(Object value) {
+    return map.containsValue(value);
+  }
+  @Override
+  public D get(Object key) {
+    return map.get(key);
+  }
+  @Override
+  public D put(K key, D value) {
+    return map.put(key, value);
+  }
+  @Override
+  public D remove(Object key) {
+    return map.remove(key);
+  }
+  @Override
+  public void putAll(Map<? extends K, ? extends D> m) {
+    map.putAll(m);
+  }
+  @Override
+  public void clear() {
+    map.clear();
+  }
+  @Override
+  public Set<K> keySet() {
+    return map.keySet();
+  }
+  @Override
+  public Collection<D> values() {
+    return map.values();
+  }
+  @Override
+  public Set<java.util.Map.Entry<K, D>> entrySet() {
+    return map.entrySet();
   }
 }
