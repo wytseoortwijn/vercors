5b2eaa6f2661adee62f573a7dbe4b6817e272997
Stefan Blom <S.C.C.Blom@utwente.nl>
2013-08-07 18:40:09 +0200

    more work on antlr v4 parsers

---
+++ src/hre/HREError.java
@@ -0,0 +1,14 @@
+package hre;
+
+public class HREError extends Error {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -1460088473914049732L;
+
+  public HREError(String format,Object...args){
+    super(String.format(format,args));
+  }
+
+}
