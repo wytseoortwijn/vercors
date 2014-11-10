fc315af69c0067a55629af0356434a73e384c52d
Stefan Blom <S.C.C.Blom@utwente.nl>
2014-11-10 10:25:44 +0100

    move local variables out of if-then-else branches for silver

---
+++ src/vct/col/rewrite/SilverReorder.java
@@ -0,0 +1,32 @@
+package vct.col.rewrite;
+
+import vct.col.ast.*;
+
+public class SilverReorder extends AbstractRewriter {
+
+  public SilverReorder(ProgramUnit source) {
+    super(source);
+  }
+
+  private BlockStatement main_block=null;
+  
+  @Override
+  public void visit(IfStatement s){
+    Warning("rewriting if");
+    BlockStatement tmp=main_block;
+    main_block=currentBlock;
+    super.visit(s);
+    main_block=tmp;
+  }
+  
+  @Override
+  public void visit(DeclarationStatement d){
+    super.visit(d);
+    if (main_block!=null){
+      Warning("rewriting if: moving decl");
+      main_block.prepend(result);
+      result=null;
+    }
+  }
+  
+}
