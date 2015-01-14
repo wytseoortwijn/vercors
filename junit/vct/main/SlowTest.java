ec5cefb56270b041e075eb7c3fd5493e3593ca11
Stefan Blom <S.C.C.Blom@utwente.nl>
2015-01-14 11:42:09 +0100

    fix magic wand example

--- junit/vct/main/SlowTest.java
+++ junit/vct/main/SlowTest.java
@@ -64,5 +64,31 @@ public class SlowTest extends ToolTest {
       sem.release();
     }
   }
+  
+  @Test
+  public void testTreeWand() {
+    sem_get(MagicWand);
+    try {
+      VCTResult res = run("vct", "--chalice", "--inline",
+          "//examples/encoding/TreeWand.java");
+      if (res.verdict != Verdict.Pass)
+        fail("bad result : " + res.verdict);
+    } finally {
+      sem.release();
+    }
+  }
+
+  @Test
+  public void testTreeWandErr() {
+    sem_get(MagicWand);
+    try {
+      VCTResult res = run("vct", "--chalice", "--inline",
+          "//examples/encoding/TreeWand-err.java");
+      if (res.verdict != Verdict.Fail)
+        fail("bad result : " + res.verdict);
+    } finally {
+      sem.release();
+    }
+  }
 
 }
