d4aed490791c8251110741de1764f9e2a8d61479
Stefan Blom <S.C.C.Blom@utwente.nl>
2015-06-12 15:20:04 +0200

    add arguments for passes

--- src/vct/main/CompilerPass.java
+++ src/vct/main/CompilerPass.java
@@ -1,9 +1,12 @@
 package vct.main;
 
+import hre.util.TestReport;
 import vct.col.ast.ProgramUnit;
 
 public abstract class CompilerPass {
 
+  public TestReport report=new TestReport();
+  
   private String description;
   
   public String getDescripion(){
@@ -14,6 +17,11 @@ public abstract class CompilerPass {
     this.description=description;
   }
   
-  public abstract ProgramUnit apply(ProgramUnit arg);
+  public abstract ProgramUnit apply(ProgramUnit arg, String ... args);
 
+  public void fail(String format,Object ... args){
+    report.fail(format,(Object[])args);
+  }
+  
+  
 }
