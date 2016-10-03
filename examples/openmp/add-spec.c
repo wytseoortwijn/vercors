0730dba5c3c3a5b03bac478e1c2a5a14f1dd2bfd
Stefan Blom <S.C.C.Blom@utwente.nl>
2016-10-03 17:03:28 +0200

    Parallel examples

---
+++ examples/openmp/add-spec.c
@@ -0,0 +1,71 @@
+// -*- tab-width:2 ; indent-tabs-mode:nil -*-
+//:: case OpenMPadd
+//:: suite puptol
+//:: tools silicon
+/*
+ * Demonstrates how two loops that must be fused to be 
+ * data race free can be specified and verified.
+ */
+#include <stdio.h>
+#include <omp.h>
+
+/*@
+  invariant a != NULL && b != NULL && c != NULL;
+  invariant len>0 && \length(a)==len && \length(b)==len && \length(c)==len;
+  context   (\forall* int k;0 <= k && k < len ; Perm(a[k],1/2));
+  context   (\forall* int k;0 <= k && k < len ; Perm(b[k],1/2));
+  context   (\forall* int k;0 <= k && k < len ; Perm(c[k],1));
+  ensures   (\forall  int k;0 <= k && k < len ; c[k]==a[k]+b[k]);
+@*/
+void add(int len,int a[],int b[],int c[]){
+  #pragma omp parallel
+  {
+    /*@
+      context (\forall* int k;0 <= k && k < len ; Perm(a[k],1/2));
+      context (\forall* int k;0 <= k && k < len ; Perm(b[k],1/2));
+      context (\forall* int k;0 <= k && k < len ; Perm(c[k],1));
+      ensures (\forall  int k;0 <= k && k < len ; c[k]==a[k]+b[k]);
+    @*/
+    #pragma omp for schedule(static) nowait
+    for(int i=0;i<len;i++)
+    /*@
+      context Perm(c[i],1) ** Perm(a[i],1/2);
+      ensures c[i] == a[i];
+    @*/
+    {
+      c[i]=a[i];
+    }
+    #pragma omp for schedule(static)
+    for(int i=0;i<len;i++)
+    /*@
+      context Perm(c[i],1) ** Perm(b[i],1/2);
+      ensures c[i] == \old(c[i]) + b[i];
+    @*/
+    {
+      c[i]=c[i]+b[i];
+    }
+  }
+}
+
+//@ requires false;
+int main(int argc, char *argv[]){
+  int a[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+  int b[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+  int c[]={-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16};
+  int i;
+  
+  printf("a: ");
+  for(i=0;i<16;i++){printf("%4d",a[i]);}
+  printf("\n");
+  printf("b: ");
+  for(i=0;i<16;i++){printf("%4d",b[i]);}
+  printf("\n");
+  
+  printf("add\n");
+  add(16,a,b,c);
+
+  printf("c: ");
+  for(i=0;i<16;i++){printf("%4d",c[i]);}
+  printf("\n");
+}
+
---
+++ examples/openmp/zero-spec.c
@@ -0,0 +1,49 @@
+// -*- tab-width:2 ; indent-tabs-mode:nil -*-
+//:: case OpenMPzero
+//:: suite puptol
+//:: tools silicon
+/*
+ * Using a parallel for loop in OpenMP to blank an array.
+ */
+#include <stdio.h>
+#include <omp.h>
+
+/*@
+  invariant a != NULL && len>0 && \length(a)==len;
+  context   (\forall* int k;0 <= k && k < len ; Perm(a[k],1));
+  ensures   (\forall  int k;0 <= k && k < len ; a[k] == 0 );
+@*/
+void zero(int len,int a[]){
+  int i;
+  #pragma omp parallel for private(i)
+  for(i=0;i<len;i++)
+  /*@
+    context Perm(a[i],1);
+    ensures a[i] == 0;
+  @*/
+  {
+    a[i]=0;
+    //@ spec_ignore {
+    printf("item %d done by thread %d.\n",i,omp_get_thread_num());
+    //@ spec_ignore }
+  }
+}
+
+//@ requires false;
+int main(int argc, char *argv[]){
+  int a[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+  int i;
+  
+  printf("a: ");
+  for(i=0;i<16;i++){printf("%4d",a[i]);}
+  printf("\n");
+  
+  printf("zero\n");
+  zero(16,a);
+
+  printf("a: ");
+  for(i=0;i<16;i++){printf("%4d",a[i]);}
+  printf("\n");
+}
+
+
