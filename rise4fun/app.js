ea164539c90026b8420ae15686e2dd8aa9559089
Wytse Oortwijn <wytseoortwijn@wlan019185.mobiel.utwente.nl>
2017-01-12 23:32:36 +0100

    minor styling

---
+++ rise4fun/app.js
@@ -0,0 +1,121 @@
+var bodyparser = require('body-parser');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var syncexec = require('sync-exec');
+var temp = require('temp');
+var app = express();
+
+// automatically track and cleanup temporary files
+temp.track();
+
+// Rise4fun claims to encode their content using GZIP, but it appears that the content is not encoded at all!
+// to prevent the bodyparser to automatically decode the apparently non-encoded contents, we remove the encoding entry from the header.
+app.use(function (req, res, next) {
+	delete req.headers["content-encoding"];
+	next();
+});
+
+// enable json and set the assets directory
+app.use(express.static('public'));
+app.use(bodyparser.json());
+
+// creates a 'servicetoolrequest' JSON object for the given example file
+load_example = function (name, filepath) {
+	var fullpath = path.join(__dirname, '../examples/', filepath);
+	return {
+		Name: name,
+		Source: fs.readFileSync(fullpath, 'utf8')
+	};
+}
+
+// default action
+app.get('/', function (req, res) {
+  res.send('Hi there! This is the Vercors interface for Rise4fun')
+});
+
+app.get('/metadata', function (req, res) {
+	// construct a metadata object by filling in the necessary fields
+	var data = new Object();
+	data.Name = "vercors";
+  data.DisplayName = "VerCors";
+  data.Version = "1.0";
+  data.Email = "w.h.m.oortwijn@utwente.nl";
+  data.SupportEmail = "w.h.m.oortwijn@utwente.nl";
+  data.TermsOfUseUrl = "http://utwente.nl/vercors?terms";
+  data.PrivacyUrl = "http://utwente.nl/vercors?privacy";
+  data.Institution = "University of Twente";
+  data.InstitutionUrl = "http://utwente.nl";
+  data.InstitutionImageUrl = req.protocol + '://' + req.host + '/fmt.png';
+  data.MimeType = "text/plain";
+	data.SupportsLanguageSyntax = false;
+  data.Title = "VerCors Verification Toolset";
+  data.Description = "Verifies memory safety and functional correctness of parallel and concurrent programs.";
+  data.Question = "Is this program functionally correct?";
+  data.Url = "http://utwente.nl/vercors";
+	data.VideoUrl = null;
+	data.DisableErrorTable = false;
+	data.Tutorials = null;
+	
+	// populate the sample programs
+	data.Samples = [
+		load_example("BasicAssert", "basic/BasicAssert.java")
+	];
+	
+	// render the metadata object as JSON
+	res.setHeader('Content-Type', 'application/json');
+	res.json(data);
+});
+
+app.post('/run', function (req, res) {
+	// is the content actually parsed with the built-in JSON parser?
+	if (req.body == undefined) {
+		res.status(400).send('Error: expecting JSON content');
+		return;
+	}
+	
+	// does the parsed JSON object has the right format?
+	if (req.body.Version == undefined || req.body.Source == undefined) {
+		res.status(400).send('Error: incorrect JSON content');
+		return;
+	}
+
+	// create a temporary file for the received (java) program
+	temp.open({ prefix: 'vercors-rise4fun', suffix: '.java' }, function (err, info) {
+		if (err) {
+			res.status(400).send('Error: could not create a temporary file');
+			console.log(err);
+			return;
+		}
+
+		// write the program to the temp file
+		fs.write(info.fd, req.body.Source);
+		fs.close(info.fd, function (err) {
+			if (err) {
+				res.status(400).send('Error: could not write to temporary file');
+				console.log(err);
+				return;
+			}
+
+			// execute vercors on the received program (with silicon)
+			var toolpath = path.join(__dirname, '../unix/bin/vct --silicon');
+			var tooloutput = syncexec(toolpath + ' ' + info.path);
+			
+			// render the output message as JSON
+			res.setHeader('Content-Type', 'application/json');
+			res.json({
+				Version: "1.0",
+				Outputs: [{ MimeType: "text/plain", Value: tooloutput.stdout }]
+			});
+		});
+	});
+});
+
+app.get('/language', function (req, res) {
+	res.sendFile(__dirname + '/public/lang.json');
+});
+
+// start the app!
+app.listen(8080, function () {
+  console.log('vercors-rise4fun interface active and listening on port 8080')
+});
