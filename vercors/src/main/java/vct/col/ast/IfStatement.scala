679727b51028bd135de8acd3c74ea35baaf2d6ab
Wytse Oortwijn <wytseoortwijn@wlan019185.mobiel.utwente.nl>
2017-01-18 13:39:36 +0100

    minor update

--- vercors/src/main/java/vct/col/ast/IfStatement.scala
+++ vercors/src/main/java/vct/col/ast/IfStatement.scala
@@ -1,7 +1,6 @@
 package vct.col.ast
 
 import hre.ast.MessageOrigin;
-import scala.collection.mutable.ArrayBuffer
 import vct.col.util.VisitorHelper
 
 object IfStatement {
@@ -9,22 +8,26 @@ object IfStatement {
 }
 
 class IfStatement extends ASTNode with VisitorHelper  {
-  private[this] val cases = ArrayBuffer[(ASTNode,ASTNode)]();
+  private[this] val cases = new java.util.ArrayList[(ASTNode,ASTNode)]()
   
   def getCount = cases.size
-  def getGuard(i:Int) = cases.apply(i)._1
-  def getStatement(i:Int) = cases.apply(i)._2
+  def getGuard(i:Int) = cases.get(i)._1
+  def getStatement(i:Int) = cases.get(i)._2
   
   def addClause(guard:ASTNode, stmt:ASTNode) : Unit = {
-    if (guard != IfStatement.elseGuard) guard.setParent(this)
     stmt.setParent(this)
-    cases += new Tuple2(guard, stmt)
+    if (guard != IfStatement.elseGuard) {
+      guard.setParent(this)
+    }
+    cases.add(new Tuple2(guard, stmt))
   }
   
   def this(cond:ASTNode, truebranch:ASTNode, falsebranch:ASTNode) = {
     this()
     addClause(cond, truebranch)
-    if (falsebranch != null) addClause(IfStatement.elseGuard, falsebranch)
+    if (falsebranch != null) {
+      addClause(IfStatement.elseGuard, falsebranch)
+    }
   }
   
   override def accept_simple[T,A](m:ASTMapping1[T,A], arg:A) = m.map(this, arg)
