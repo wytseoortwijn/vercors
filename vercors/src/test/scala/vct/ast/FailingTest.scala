1affc9380f68c49e1a83576c312a951212e04e79
Pieter Bos <info@pieterbos.me>
2019-03-07 11:46:29 +0100

    Remove failing test

--- vercors/src/test/scala/vct/ast/FailingTest.scala
+++
@@ -1,9 +0,0 @@
-package vct.ast
-
-import org.scalatest._
-
-class FailingTest extends FlatSpec with Matchers {
-    "This test" should "deliberately fail" in {
-        false should be (true)
-    }
-}
