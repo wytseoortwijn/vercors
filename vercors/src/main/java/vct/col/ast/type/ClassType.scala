b5187309722393c59b822aa52bc8d285fb56619f
Pieter Bos <info@pieterbos.me>
2019-07-15 14:08:08 +0200

    fix extraneous debug parameter

--- vercors/src/main/java/vct/col/ast/type/ClassType.scala
+++ vercors/src/main/java/vct/col/ast/type/ClassType.scala
@@ -109,6 +109,6 @@ case class ClassType(val names:List[String], val params:List[ASTNode]) extends T
   override def accept_simple[T](m:ASTMapping[T]) = handle_standard(() => m.map(this))
   override def accept_simple[T](m:TypeMapping[T]) = handle_standard(() => m.map(this))
 
-  override def debugTreeChildrenFields: Iterable[String] = Seq("names", "params", "args")
+  override def debugTreeChildrenFields: Iterable[String] = Seq("names", "args")
   override def debugTreePropertyFields: Iterable[String] = Seq()
 }
